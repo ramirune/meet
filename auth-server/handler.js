ddcfc12d00f8d0b78da7d1375d07cc99941f1cc4
ramirune <timpiga3@hotmail.com>
2021-11-27 17:33:57 +0100

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,5 +1,5 @@
 const { google } = require("googleapis");
-//const { resultingClientExists } = require("workbox-core/_private");
+
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
 /**
