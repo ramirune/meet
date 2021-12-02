663ef2a258299ab85820df3bd8000269d8e50ece
ramirune <timpiga3@hotmail.com>
2021-12-02 17:32:31 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -16,7 +16,7 @@ export const extractLocations = (events) => {
   return locations;
 };
 
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
@@ -94,7 +94,7 @@ export const getAccessToken = async () => {
       const { authUrl } = results.data;
       return (window.location.href = authUrl);
     }
-    return code && getAccessToken(code);
+    return code && getToken(code);
   }
   return accessToken;
 }
