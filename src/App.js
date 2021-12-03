b54ec002f988454bcaabca5959c79e9793663051
ramirune <timpiga3@hotmail.com>
2021-12-03 17:15:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,14 +27,15 @@ class App extends Component {
     this.mounted = false;
   }
 
-  updateEvents = (location) => {
+  updateEvents = (location, eventCount) => {
     getEvents().then((events) => {
       const locationEvents = (location === 'all') ?
         events :
         events.filter((event) => event.location === location);
-      const { numberOfEvents } = this.state;
+
       this.setState({
-        events: locationEvents.slice(0, numberOfEvents)
+        events: locationEvents.slice(0, eventCount),
+        currentLocation: location
       });
     });
   }
