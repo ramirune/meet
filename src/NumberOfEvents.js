ba83b15e1d2cc67069508021fb410098aca12d41
ramirune <timpiga3@hotmail.com>
2021-12-03 21:44:39 +0100

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 class NumberOfEvents extends Component {
-  state = {
+  /* state = {
     numberOfEvents: 32,
   };
 
@@ -17,16 +17,16 @@ class NumberOfEvents extends Component {
       });
       this.props.updateNumberofEvents(number);
     }
-  };
+  }; */
 
   render() {
     return (
       <div>
         <p>Number of events to show:</p>
         <input type="number"
-          value={this.state.numberOfEvents}
+          value={this.props.numberOfEvents}
           className="number-of-events"
-          onChange={(e) => this.handleInputChanged(e)} />
+          onChange={(e) => this.props.updateNumberOfEvents(e)} />
       </div>
     );
   }
