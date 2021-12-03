8ede327346286c6784b2de30feeffa388332e174
ramirune <timpiga3@hotmail.com>
2021-12-03 21:26:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -52,13 +52,15 @@ class App extends Component {
   }
 
   getData = () => {
-    const { locations, events } = this.state;
-    const data = locations.map((location) => {
-      const number = events.filter((event) => event.location === location).length
-      const city = location.split(', ').shift();
-      return { city, number };
-    });
-    return data;
+    if (this.mounted) {
+      const { locations, events } = this.state;
+      const data = locations.map((location) => {
+        const number = events.filter((event) => event.location === location).length
+        const city = location.split(', ').shift();
+        return { city, number };
+      });
+      return data;
+    }
   };
 
   render() {
