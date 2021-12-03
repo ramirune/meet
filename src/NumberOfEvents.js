c1a3e9441d34296a7f547f35923aeca8f7762428
ramirune <timpiga3@hotmail.com>
2021-12-03 19:09:11 +0100

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -14,7 +14,7 @@ class NumberOfEvents extends Component {
       this.setState({
         numberOfEvents: number,
       });
-      this.props.updateNumberofEvents(number);
+      //this.props.updateNumberofEvents(number);
     }
   };
 
