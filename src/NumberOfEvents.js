d2a8e430a6fdce647e527dd6381154474830c52c
ramirune <timpiga3@hotmail.com>
2021-12-03 20:07:19 +0100

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -15,7 +15,7 @@ class NumberOfEvents extends Component {
       this.setState({
         numberOfEvents: value,
       });
-      this.updateNumberofEvents(value);
+      this.props.updateNumberofEvents(value);
     }
   };
 
@@ -23,7 +23,7 @@ class NumberOfEvents extends Component {
     return (
       <div>
         <p>Number of events to show:</p>
-        <input type="number" value={this.state.numberOfEvents} className="number-of-events" onChange={(e) => this.handleInputChange(e)} />
+        <input type="number" value={this.state.numberOfEvents} className="number-of-events" onChange={this.updateNumberofEvents} />
       </div>
     );
   }
