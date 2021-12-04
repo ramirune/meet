be4b083f12d0b89afa9a17b0d2924d8afc494a06
ramirune <timpiga3@hotmail.com>
2021-12-04 16:24:47 +0100

    style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './nprogress.css';
 import { Container, Row, Col } from "react-bootstrap";
-import './styles/App.scss';
+import './App.css';
 
 import EventList from './EventList';
 import CitySearch from './CitySearch';
