78d63e8fde12b845714bcc5534744747f764a9b4
ramirune <timpiga3@hotmail.com>
2021-12-04 16:45:37 +0100

    style

--- src/EventList.js
+++ src/EventList.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 import Event from './Event';
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
+import './styles/EventList.scss';
 
 class EventList extends Component {
   render() {
     const { events } = this.props;
     return (
-      <Row>
-        <Col md={10} sm={12} className="event-list">
-          <ul className="EventList row">
-            {events.map(event =>
-              <li key={event.id} className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                <Event event={event} />
-              </li>
-            )}
-          </ul>
-        </Col>
-      </Row>
+      <Container className="eventlist-container">
+        <Row className="event-list">
+          <Col md={10} sm={12}>
+            <ul className="EventList row">
+              {events.map(event =>
+                <li key={event.id} className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+                  <Event event={event} />
+                </li>
+              )}
+            </ul>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
