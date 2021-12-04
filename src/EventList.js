7518305465323275d48bf3e1650a627a909af4cb
ramirune <timpiga3@hotmail.com>
2021-12-04 16:41:34 +0100

    style

--- src/EventList.js
+++ src/EventList.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import Event from './Event';
+import { Row, Col } from "react-bootstrap";
 
 class EventList extends Component {
   render() {
     const { events } = this.props;
     return (
-      <ul className="EventList">
-        {events.map(event =>
-          <li key={event.id}>
-            <Event event={event} />
-          </li>
-        )}
-      </ul>
+      <Row>
+        <Col md={10} sm={12} className="event-list">
+          <ul className="EventList row">
+            {events.map(event =>
+              <li key={event.id} className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+                <Event event={event} />
+              </li>
+            )}
+          </ul>
+        </Col>
+      </Row>
     );
   }
 }
