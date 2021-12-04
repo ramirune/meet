2c1e1c7fde39868d976a08b10e535595a03d958b
ramirune <timpiga3@hotmail.com>
2021-12-04 15:22:11 +0100

    style

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
 import logo from "./img/meet.png";
 import './styles/Header.scss';
+import { Row, Col, Container } from "react-bootstrap";
 
 function Header(prop) {
   return (
-    <div className="logo">
-      <img src={logo} alt="Meet app logo" />
-    </div>
+    <Container className="logo-container">
+      <Row>
+        <Col />
+        <Col xs={6}>
+          <img src={logo} alt="Meet app logo" className="logo" />
+        </Col>
+        <Col />
+      </Row>
+    </Container>
   );
 }
 
