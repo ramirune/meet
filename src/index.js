707c44a884db8873d90952733275514d10488392
ramirune <timpiga3@hotmail.com>
2021-12-09 16:04:40 +0100

    atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+
+atatus.config('5ccb9ea3aec14cd6a9764155fb7b05de').install();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -21,3 +25,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.notify(new Error('Test Atatus Setup'));
