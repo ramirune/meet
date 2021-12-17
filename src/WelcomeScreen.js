6608593de2a277c1b82f2e17781104b19c2aee8e
ramirune <timpiga3@hotmail.com>
2021-12-17 21:41:07 +0100

    commit

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -1,41 +1,49 @@
 import React from 'react';
 import './styles/WelcomeScreen.css';
+import { Row, Col, Container } from 'react-bootstrap';
+import welcomeLogo from './img/logo_welcome.png';
 
 function WelcomeScreen(props) {
 	return props.showWelcomeScreen ? (
-		<div className="WelcomeScreen">
-			<h1>Welcome to the Meet app</h1>
-			<h4>
-				Log in to see upcoming events around the world for full-stack developers
-			</h4>
-			<div className="button_cont" align="center">
-				<div class="google-btn">
-					<div class="google-icon-wrapper">
-						<img
-							class="google-icon"
-							src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
+		<Container className="WelcomeScreen" fluid>
+			<Row>
+				<Col>
+					<img src={welcomeLogo} alt="welcome screen logo" />
+					<h1>Welcome to the Meet app</h1>
+					<h4>
+						Log in to see upcoming events around the world for full-stack
+						developers
+					</h4>
+					<div className="button_cont" align="center">
+						<div class="google-btn">
+							<div class="google-icon-wrapper">
+								<img
+									class="google-icon"
+									src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
 o.svg"
-							alt="Google sign-in"
-						/>
+									alt="Google sign-in"
+								/>
+							</div>
+							<button
+								onClick={() => {
+									props.getAccessToken();
+								}}
+								rel="nofollow noopener"
+								class="btn-text"
+							>
+								<b>Sign in with google</b>
+							</button>
+						</div>
 					</div>
-					<button
-						onClick={() => {
-							props.getAccessToken();
-						}}
+					<a
+						href="https://YOUR_GITHUB_USERNAME.github.io/meet/privacy.html"
 						rel="nofollow noopener"
-						class="btn-text"
 					>
-						<b>Sign in with google</b>
-					</button>
-				</div>
-			</div>
-			<a
-				href="https://YOUR_GITHUB_USERNAME.github.io/meet/privacy.html"
-				rel="nofollow noopener"
-			>
-				Privacy policy
-			</a>
-		</div>
+						Privacy policy
+					</a>
+				</Col>
+			</Row>
+		</Container>
 	) : null;
 }
 
