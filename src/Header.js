fe0eb87903858658bf4ba7ca2dbdb76d0a58d25f
ramirune <timpiga3@hotmail.com>
2021-12-18 21:47:04 +0100

    commit

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,20 @@
-import React from "react";
-import logo from "./img/meet.png";
+import React from 'react';
+import logo from './img/logo-welcome.png';
 import './styles/Header.scss';
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container } from 'react-bootstrap';
 
 function Header(prop) {
-  return (
-    <Container className="logo-container" fluid>
-      <Row>
-        <Col />
-        <Col xs={12}>
-          <img src={logo} alt="Meet app logo" className="logo" />
-        </Col>
-        <Col />
-      </Row>
-    </Container>
-  );
+	return (
+		<Container className="logo-container" fluid>
+			<Row>
+				<Col />
+				<Col xs={12}>
+					<img src={logo} alt="Meet app logo" className="logo" />
+				</Col>
+				<Col />
+			</Row>
+		</Container>
+	);
 }
 
 export default Header;
