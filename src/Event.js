d16cd74f82179438970d1dce5272ed3af44b0f17
ramirune <timpiga3@hotmail.com>
2021-12-18 21:45:32 +0100

    commit

--- src/Event.js
+++ src/Event.js
@@ -1,48 +1,63 @@
-import React, { Component } from "react";
-import { Button, Card } from "react-bootstrap";
+import React, { Component } from 'react';
+import { Button, Card } from 'react-bootstrap';
 import './styles/Event.scss';
 
 class Event extends Component {
-  state = {
-    event: {},
-    collapsed: true,
-  };
+	state = {
+		event: {},
+		collapsed: true,
+	};
 
-  handleClick = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  };
+	handleClick = () => {
+		this.setState({
+			collapsed: !this.state.collapsed,
+		});
+	};
 
-  render() {
-    const { event } = this.props;
-    const { collapsed } = this.state;
-    return (
-      <Card className="event">
-        <Card.Header className="summary" as="h4">{event.summary}</Card.Header>
-        <Card.Body className="event-body">
-          <p className="start-date">{event.start.dateTime} ({event.start.timeZone})</p>
+	render() {
+		const { event } = this.props;
+		const { collapsed } = this.state;
+		return (
+			<Card className="event">
+				<Card.Header className="summary" as="h4">
+					{event.summary}
+				</Card.Header>
+				<Card.Body className="event-body">
+					<p className="start-date">
+						{event.start.dateTime} ({event.start.timeZone})
+					</p>
 
-          <p className="location">@{event.summary} | {event.location}</p>
+					<p className="location">
+						@{event.summary} | {event.location}
+					</p>
 
-          {!collapsed &&
-            <div className={`extra-details ${this.state.collapsed ? "hide" : "show"}`}>
-              <br />
-              <h6 className="about">About Event</h6>
-              <a href={event.htmlLink} target="_blank" rel="noreferrer">
-                See deatails on Google calendar
-              </a>
-              <p className="event-description">{event.description}</p>
-
-            </div>
-          }
-          <Button variant="light" size="md" id="eventButton" className={`${collapsed ? "show" : "hide"}-details-btn`} onClick={this.handleClick}>
-            {collapsed ? "show Details" : "Hide Details"}
-          </Button>
-        </Card.Body>
-      </Card>
-    );
-  }
+					{!collapsed && (
+						<div
+							className={`extra-details ${
+								this.state.collapsed ? 'hide' : 'show'
+							}`}
+						>
+							<br />
+							<h6 className="about">About Event</h6>
+							<a href={event.htmlLink} target="_blank" rel="noreferrer">
+								See deatails on Google calendar
+							</a>
+							<p className="event-description">{event.description}</p>
+						</div>
+					)}
+					<Button
+						variant="light"
+						size="md"
+						id="eventButton"
+						className={`${collapsed ? 'show' : 'hide'}-details-btn`}
+						onClick={this.handleClick}
+					>
+						{collapsed ? 'show Details' : 'Hide Details'}
+					</Button>
+				</Card.Body>
+			</Card>
+		);
+	}
 }
 
 export default Event;
