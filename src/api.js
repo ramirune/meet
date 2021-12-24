e9dd00e038370818978ca36da750f19209624425
ramirune <timpiga3@hotmail.com>
2021-12-24 17:07:36 +0100

    commit

--- src/api.js
+++ src/api.js
@@ -43,7 +43,9 @@ const removeQuery = () => {
 const getToken = async code => {
 	const encodeCode = encodeURIComponent(code);
 	const { access_token } = await fetch(
-		`https://j34cd6uik1.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`
+		'https://j34cd6uik1.execute-api.eu-central-1.amazonaws.com/dev/api/token' +
+			'/' +
+			encodeCode
 	)
 		.then(res => {
 			return res.json();
@@ -72,7 +74,10 @@ export const getEvents = async () => {
 	const token = await getAccessToken();
 	if (token) {
 		removeQuery();
-		const url = `https://j34cd6uik1.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/${token}`;
+		const url =
+			'https://j34cd6uik1.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' +
+			'/' +
+			token;
 		const result = await axios.get(url);
 		if (result.data) {
 			var locations = extractLocations(result.data.events);
